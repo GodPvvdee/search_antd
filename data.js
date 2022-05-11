10b17484a9bd2ea4bd4b69d70f415ba7c45537cf
pvvdee <56915962+GodPvvdee@users.noreply.github.com>
2022-05-11 16:05:13 +0800

    Initial commit

---
+++ data.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    fieldtext: {
+      key: "1",
+      name: "John Brown",
+      age: 32,
+      address: "New York No. 1 Lake Park",
+      x: "extra1"
+    }
+  },
+  {
+    fieldtext: {
+      key: "2",
+      name: "Jim Green",
+      age: 42,
+      address: "London No. 1 Lake Park",
+      x: "extra2"
+    }
+  },
+  {
+    fieldtext: {
+      key: "3",
+      name: "Joe Black",
+      age: 32,
+      address: "Sidney No. 1 Lake Park",
+      x: "extra2"
+    }
+  },
+  {
+    fieldtext: {
+      key: "4",
+      name: "Jim Red",
+      age: 32,
+      address: "London No. 2 Lake Park",
+      x: "extra3"
+    }
+  }
+];
